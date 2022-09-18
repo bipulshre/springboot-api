648615de92347a59562829877d930792ea816598
bipulshre <bipulshr28@Gmail.com>
2022-09-18 17:49:15 +0545

    json

--- src/main/java/com/example/springdemo/SpringDemoApplication.java
+++ src/main/java/com/example/springdemo/SpringDemoApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @SpringBootApplication
 @RestController
 public class SpringDemoApplication {
@@ -14,7 +16,7 @@ public class SpringDemoApplication {
     }
 
     @GetMapping
-    public String hello(){
-        return "hello world this is my  first spring framework app";
+    public List<String> hello(){
+        return List.of("hello", "world","this", "is" ,"my" , "first spring", "framework app");
     }
 }
