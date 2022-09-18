a19ccee6f5903c8c81b9466e9054f1ceb0c9d9d4
bipulshre <bipulshr28@Gmail.com>
2022-09-18 21:07:00 +0545

    space added

--- src/main/java/com/example/springdemo/student/StudentService.java
+++ src/main/java/com/example/springdemo/student/StudentService.java
@@ -12,6 +12,7 @@ import java.util.List;
 @Service
 public class StudentService {
     public List<Student> getStudents(){
+
         return List.of(
                 new Student(
                         1L,
