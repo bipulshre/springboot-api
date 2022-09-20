dcddecf994cc8ea16a6897a951293be5a75bacdc
bipulshre <bipulshr28@Gmail.com>
2022-09-20 22:19:06 +0545

    learned some business logic

--- src/main/java/com/example/springdemo/student/StudentService.java
+++ src/main/java/com/example/springdemo/student/StudentService.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class StudentService {
@@ -21,7 +22,14 @@ public class StudentService {
     }
 
     public void addNewStudent(Student student){
-        System.out.println(student);
+
+        Optional<Student> studentOptional= studentRepository
+                .findStudentByEmail(student.getEmail());
+
+        if(studentOptional.isPresent()){
+            throw new IllegalStateException("email taken");
+        }
+        studentRepository.save(student);
     }
 
     public List<Student> getStudents(){
