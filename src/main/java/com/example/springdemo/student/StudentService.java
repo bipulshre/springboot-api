6ee7c63fb9960f6ed30568f8d20b7165db650895
bipulshre <bipulshr28@Gmail.com>
2022-09-20 10:29:32 +0545

    post mapping

--- src/main/java/com/example/springdemo/student/StudentService.java
+++ src/main/java/com/example/springdemo/student/StudentService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.time.LocalDate;
 import java.time.Month;
@@ -17,6 +18,10 @@ public class StudentService {
     @Autowired
     public StudentService(StudentRepository studentRepository) {
         this.studentRepository = studentRepository;
+    }
+
+    public void addNewStudent(Student student){
+        System.out.println(student);
     }
 
     public List<Student> getStudents(){
